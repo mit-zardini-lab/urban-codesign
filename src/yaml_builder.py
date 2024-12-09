ddd05bd2421383d40fb9ca490c27dbec3c1b70eb
Deniz Aydemir <deniz@aydemir.us>
2024-12-09 06:42:11 -0500

    some more fixes

--- src/yaml_builder.py
+++ src/yaml_builder.py
@@ -72,7 +72,7 @@ def generate_cost_yaml(layouts: list[Layout], output_file: str) -> None:
                 f"{layout.pretty_flat.count("B")} Nat",
                 f"{layout.cost_yearly} $",
                 f"{layout.co2_cost_upfront} Nat",
-                f"{layout.co2_absorption_yearly - layout.co2_cost_yearly} Int"
+                f"{layout.co2_cost_yearly + layout.co2_absorption_yearly} Int"
             ]
         }
         yaml_data["implementations"][layout.pretty_flat] = layout_data
